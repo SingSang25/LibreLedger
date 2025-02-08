a579b95e67b27bae8039305e6427c105d3f5a275
Jan Zeugin <jan.zeugin@outlook.com>
2025-02-08 15:52:03 +0100

    Projektdateien hinzufügen.

---
+++ LibreLedger/LibreLedger.cpp
@@ -0,0 +1,12 @@
+﻿// LibreLedger.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "LibreLedger.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
